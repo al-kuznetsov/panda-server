49c05a7c721a0d0b0cd0d5facad8383309411c30
al-kuznetsov <alkuznetsov@aol.com>
2022-10-23 23:57:18 +0500

    Add CRUD to AnimalRestController

--- src/main/java/com/aol/alkuznetsov/panda/server/rest/AnimalRestController.java
+++ src/main/java/com/aol/alkuznetsov/panda/server/rest/AnimalRestController.java
@@ -4,7 +4,12 @@ import com.aol.alkuznetsov.panda.server.dto.AnimalDto;
 import com.aol.alkuznetsov.panda.server.service.AnimalService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,20 @@ public class AnimalRestController {
   @GetMapping
   public List<AnimalDto> findAll() {
     return animalService.findAll();
+  }
+
+  @PostMapping
+  public AnimalDto create(@RequestBody AnimalDto animalDto) {
+    return animalService.create(animalDto);
+  }
+
+  @PutMapping("/{id}")
+  public AnimalDto update(@PathVariable Long id, @RequestBody AnimalDto animalDto) {
+    return animalService.update(id, animalDto);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteById(@PathVariable Long id) {
+    animalService.deleteById(id);
   }
 }
--- src/main/java/com/aol/alkuznetsov/panda/server/service/AnimalService.java
+++ src/main/java/com/aol/alkuznetsov/panda/server/service/AnimalService.java
@@ -1,14 +1,18 @@
 package com.aol.alkuznetsov.panda.server.service;
 
 import com.aol.alkuznetsov.panda.server.dto.AnimalDto;
+import com.aol.alkuznetsov.panda.server.exception.DataNotFoundException;
 import com.aol.alkuznetsov.panda.server.mapper.AnimalMapper;
+import com.aol.alkuznetsov.panda.server.model.Animal;
 import com.aol.alkuznetsov.panda.server.repository.AnimalRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class AnimalService {
@@ -17,8 +21,40 @@ public class AnimalService {
 
   @Transactional(readOnly = true)
   public List<AnimalDto> findAll() {
+    log.debug("Retrieving the list of all animals as AnimalDtos");
     return animalRepository.findAll().stream()
         .map(animalMapper::toDto)
         .collect(Collectors.toList());
+  }
+
+  @Transactional
+  public AnimalDto create(AnimalDto animalDto) {
+    log.debug("Creating new Animal based on AnimalDto {}", animalDto);
+    Animal savedAnimal = animalRepository.save(animalMapper.fromDto(animalDto));
+    log.debug("Saved new Animal {}", savedAnimal);
+    return animalMapper.toDto(savedAnimal);
+  }
+
+  @Transactional
+  public AnimalDto update(Long id, AnimalDto animalDto) {
+    log.debug("Updating Animal with id {} based on AnimalDto {}", id, animalDto);
+    if (!animalRepository.existsById(id)) {
+      throw new DataNotFoundException("Animal not found in Database for id: " + id);
+    }
+    Animal animal = animalMapper.fromDto(animalDto);
+    animal.setId(id);
+    Animal updatedAnimal = animalRepository.save(animal);
+    log.debug("Updated Animal {}", updatedAnimal);
+    return animalMapper.toDto(updatedAnimal);
+  }
+
+  @Transactional
+  public void deleteById(Long id) {
+    log.debug("Deleting Animal with id {}", id);
+    if (!animalRepository.existsById(id)) {
+      throw new DataNotFoundException("Animal not found in Database for id: " + id);
+    }
+    animalRepository.deleteById(id);
+    log.debug("Deleted Animal with id {}", id);
   }
 }
